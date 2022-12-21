04b5d55dcec35c3e7f680aa1a9f9d9a0f09607f6
github <93103899+Kangsukbum@users.noreply.github.com>
2022-12-21 13:04:06 +0900

    d
    d

---
+++ app/1.js
@@ -0,0 +1,11 @@
+const musicList=[
+    {name:"Alone",artist="color Out", img:"album1.jpg",audio:"audio:Alone"},
+    {name:"Mahidevran",artist="Maze of sorrow -- Mahidevran rock band", img:"album2.jpg",audio:"audio:Mahidevran"},
+
+    {name:"No Rest Or Endless Rest",artist=" Lisofv", img:"album3.jpg",audio:"audio:Mahidevran-Maze_of_sorrow"},
+    {name:"Old Melody",artist="tab", img:"album4.jpg",audio:"audio:Old Melody"},
+    {name:"Tantalizing Youth",artist="Social Square", img:"album5.jpg",audio:"audio:Tantalizing Youth"},
+    {name:"The Deep",artist="Anitek", img:"album6.jpg",audio:"audio:The Deep"},
+
+
+]
